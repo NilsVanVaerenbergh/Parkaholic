f97e3d7bafa4c88c5fa2670bed51f7578ecdd58f
rcelen <74794010+rcelen@users.noreply.github.com>
2023-05-22 16:34:36 +0200

    added leaveParkingSpot functionality (#13)

--- src/lib/parkingSpot.dart
+++ src/lib/parkingSpot.dart
@@ -4,48 +4,71 @@
 
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 ParkingSpot parkingSpotFromJson(String str) => ParkingSpot.fromJson(json.decode(str));
 
 String parkingSpotToJson(ParkingSpot data) => json.encode(data.toJson());
 
 class ParkingSpot {
-    // String id;
+    String id;
     bool inUse;
     double lat;
     double lng;
     int size;
-    // String car;
-    // int? timeOfLeaving;
-    // int? leavingIn;
+    DocumentReference carId;
+    String address;
+    int? timeOfLeaving;
+    int? availableIn;
 
     ParkingSpot({
-        // required this.id,
+        required this.id,
         required this.inUse,
         required this.lat,
         required this.lng,
         required this.size,
-        // required this.car,
+        required this.carId,
+        required this.address,
+        this.availableIn,
+        this.timeOfLeaving
     });
 
     factory ParkingSpot.fromJson(Map<String, dynamic> json) => ParkingSpot(
-        // id: json["id"],
-        inUse: json["InUse"],
-        lat: json["Lat"]?.toDouble(),
-        lng: json["Lng"]?.toDouble(),
-        size: json["Size"],
-        // car: json["Car"],
+        carId: json["car"],
+        inUse: json["inUse"],
+        lat: json["lat"]?.toDouble(),
+        lng: json["lng"]?.toDouble(),
+        size: json["size"],
+        id: json["id"],
+        address: json["address"],
+        availableIn: json["availableIn"],
+        timeOfLeaving: json["timeOfLeaving"]
     );
 
     Map<String, dynamic> toJson() => {
-        // "id": id,
-        "InUse": inUse,
-        "Lat": lat,
-        "Lng": lng,
-        "Size": size,
-        // "Car": car,
+        "car": carId,
+        "inUse": inUse,
+        "lat": lat,
+        "lng": lng,
+        "size": size,
+        "id": id,
+        "address": address,
+        "availableIn": availableIn,
+        "timeOfLeaving": timeOfLeaving,
     };
 
-    leaveParkingSpot(ParkingSpot parkingSpot){
-      
-    }
+    void leaveParkingSpot(int leavingIn) {
+      DocumentReference docUser = FirebaseFirestore.instance.collection("ParkingSpots").doc(id);
+        docUser.update({
+          "inUse": false,
+          "availableIn": leavingIn,
+          "timeOfLeaving": DateTime.now().microsecondsSinceEpoch,
+        }).then((_) {
+        // Update completed
+        print("Parking spot updated successfully.");
+        }).catchError((error) {
+        // Error occurred while updating
+      print("Error updating parking spot: $error");
+  });
+}
 }
