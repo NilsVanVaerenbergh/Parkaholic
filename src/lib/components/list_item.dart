b56a22b12bede7f83da76d46d504ba7cdea95c9d
Nils Van Vaerenbergh <96132018+NilsVanVaerenbergh@users.noreply.github.com>
2023-05-23 11:51:34 +0200

    added list of cars

---
+++ src/lib/components/list_item.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../pages/cars.dart';
+
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context, QueryDocumentSnapshot userData);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+class CarItem implements ListItem {
+  final String manufacturer;
+  final String model;
+  final String id;
+
+  CarItem({required this.manufacturer, required this.model, required this.id});
+
+  @override
+  Widget buildTitle(BuildContext context, QueryDocumentSnapshot userData) {
+    return Row(children: [
+      Text(manufacturer),
+      const Spacer(),
+      IconButton(
+          color: Colors.red,
+          onPressed: () => {
+                FirebaseFirestore.instance.collection("Cars").doc(id).delete(),
+                Navigator.pop(context),
+                Navigator.pop(context),
+                Navigator.pop(context),
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Cars(
+                              userData: userData,
+                            ))),
+              },
+          icon: const Icon(Icons.remove_circle))
+    ]);
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) {
+    return Text(model);
+  }
+}
