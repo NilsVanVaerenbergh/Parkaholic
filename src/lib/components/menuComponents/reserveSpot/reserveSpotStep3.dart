16b01a4174863510340421458ccb6e8e0921b8d6
Nils Van Vaerenbergh <96132018+NilsVanVaerenbergh@users.noreply.github.com>
2023-05-23 15:29:12 +0200

    fix typo

--- src/lib/components/menuComponents/reserveSpot/reserveSpotStep3.dart
+++ src/lib/components/menuComponents/reserveSpot/reserveSpotStep3.dart
@@ -5,10 +5,9 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:src/car.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class ReserveSpotStep3 extends StatefulWidget {
   const ReserveSpotStep3({super.key, required this.carId});
-  
+
   final String carId;
 
   @override
@@ -16,29 +15,27 @@ class ReserveSpotStep3 extends StatefulWidget {
 }
 
 class _ReserveSpotStep3State extends State<ReserveSpotStep3> {
-
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         const Text("Confirmation"),
         const Text("The spot is yours!"),
-        SizedBox(height: 20,),
+        SizedBox(
+          height: 20,
+        ),
         Text("This car used the spot before you."),
         FutureBuilder<Car?>(
           future: getPreviousCar(),
-          builder: (context, snapshot){
-            if (snapshot.hasError){
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
               return Text("Something went wrong. No car info available");
-            }
-            else if (snapshot.hasData){
+            } else if (snapshot.hasData) {
               final car = snapshot.data;
               return car == null
-                ? Center(child:Text('No car info'))
-                : buildCarInfo(car);
-            }
-            else{
+                  ? Center(child: Text('No car info'))
+                  : buildCarInfo(car);
+            } else {
               return Text("Geen informatie over deze auto");
             }
           },
@@ -47,24 +44,30 @@ class _ReserveSpotStep3State extends State<ReserveSpotStep3> {
     );
   }
 
-  Future<Car?> getPreviousCar() async{
-    final carDoc = FirebaseFirestore.instance.collection("Cars").doc(widget.carId);
+  Future<Car?> getPreviousCar() async {
+    final carDoc =
+        FirebaseFirestore.instance.collection("Cars").doc(widget.carId);
     final snapshot = await carDoc.get();
-    if (snapshot.exists){
+    if (snapshot.exists) {
       return Car.fromJson(snapshot.data()!);
     }
   }
-  Widget buildCarInfo(Car? car){
+
+  Widget buildCarInfo(Car? car) {
     return Row(
       children: [
-        SvgPicture.asset("car.svg",semanticsLabel: "Car", width: 100, height: 100,),
+        SvgPicture.asset(
+          "car.svg",
+          semanticsLabel: "Car",
+          width: 100,
+          height: 100,
+        ),
         Column(
           children: [
             Text("Model: ${car!.manufacturer} ${car.model}"),
-            Text("Color ${car.color}")
+            Text("Color: ${car.color}")
           ],
         )
-        
       ],
     );
   }
