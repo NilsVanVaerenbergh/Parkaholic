c09c051dd3b7ed542ccbd6984352e0c8ecc48c52
rcelen <robbert.celen@hotmail.be>
2023-05-24 10:55:37 +0200

    added dateTime formatting for leaveSpot2

--- src/lib/components/menuComponents/leaveSpot/leaveSpotStep2.dart
+++ src/lib/components/menuComponents/leaveSpot/leaveSpotStep2.dart
@@ -6,38 +6,58 @@ class LeaveSpotStep2 extends StatefulWidget {
   const LeaveSpotStep2({super.key, required this.leavingIn});
 
   final int leavingIn;
-
+  
   @override
   State<LeaveSpotStep2> createState() => _LeaveSpotStep2State();
 }
 
 class _LeaveSpotStep2State extends State<LeaveSpotStep2> {
+  
+  String leavingString = "";
+
+  @override
+  void initState() {
+     super.initState();
+    DateTime leavingDate = DateTime.fromMicrosecondsSinceEpoch(widget.leavingIn);
+    leavingString = "${twoDigits(leavingDate.day)}/${twoDigits(leavingDate.month)}/${leavingDate.year} ${twoDigits(leavingDate.hour)}:${twoDigits(leavingDate.minute)}";
+  }
+  
+  String twoDigits(int n) {
+    if (n >= 10) {
+      return "$n";
+    }
+    return "0$n";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const SizedBox(height: 25,),
+        const SizedBox(height: 20,),
+        Text(
+          "Drive safe!",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 20,),
         Container(
-          decoration: BoxDecoration(
-              color: Colors.grey.shade400, borderRadius: BorderRadius.circular(8)),
           padding: EdgeInsets.all(25),
           margin: EdgeInsets.symmetric(horizontal: 50),
           child: Center(
             child: Text.rich(
               TextSpan(
-                text: "You will be leaving in ",
+                text: "You will be leaving on ",
                 style: TextStyle(fontSize: 15),
                 children: <TextSpan>[
                   TextSpan(
-                    text: widget.leavingIn.toString() + " minutes",
-                    style: TextStyle(fontSize: 15),
+                    text: leavingString,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                   )
                 ]
               ),
             ),
           ),
         ),
-        const SizedBox(height: 25,),
+        const SizedBox(height: 20,),
       ],
     );
   }
